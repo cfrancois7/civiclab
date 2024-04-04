01042d941bda4c6d547c0156695f519584253b3a
Cyril François <cyril.francois.87@gmail.com>
2024-04-05 01:18:46 +0200

    new config for azure web app

--- az_config.sh
+++ az_config.sh
@@ -40,4 +40,4 @@ RUNTIME="PYTHON|3.11"
 # browse to the site
 # az webapp browse --name $SITENAME --resource-group $RESOURCEGROUP
 
-az webapp config set --resource-group $RESOURCEGROUP --name $SITENAME --startup-file "python -m ./src/app.py"
+az webapp config set --resource-group $RESOURCEGROUP --name $SITENAME --startup-file "startup.sh"
