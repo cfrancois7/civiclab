37b8985fa876f0ef2ad476aa8835e58f4f7ab0e1
Cyril François <cyril.francois.87@gmail.com>
2024-04-05 10:18:50 +0200

    change port

--- startup.sh
+++ startup.sh
@@ -1,1 +1,2 @@
-uvicorn src.main:app --reload --log-level debug --port 8000
+uvicorn src.main:app --reload --log-level debug --port 8080
+# gunicorn src.main:app --bind 0.0.0.0 --reload --log-level debug
